98ca69451f35ef7a6a1fa23d47b91249987ab2ed
Gavin Perez <gavinperez1518@gmail.com>
2016-11-18 14:02:42 -0800

    part5

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,9 +4,31 @@ var namespace = "http://www.w3.org/2000/svg"
 var canvas = document.getElementById("game-board")
 var circle = document.createElementNS(namespace, "circle")
 
-function changeshape() {
+function makeShape() {
 circle.setAttribute("cx", 40)
 circle.setAttribute("cy", 40)
 circle.setAttribute("r", 30)
 circle.setAttribute("fill", "blue")
+ canvas.appendChild(circle)
 }
+function tapcircle(){
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 40)
+circle.setAttribute("r", 30)
+circle.setAttribute("fill", "red")
+ canvas.appendChild(circle)
+}
+function switchcircle(){
+  circle.setAttribute("cx", 250)
+  circle.setAttribute("cy", 40)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "blue")
+   canvas.appendChild(circle)
+  }
+  function middlecircle(){
+    circle.setAttribute("cx", 40)
+    circle.setAttribute("cy", 140)
+    circle.setAttribute("r", 30)
+    circle.setAttribute("fill", "red")
+     canvas.appendChild(circle)
+    }
